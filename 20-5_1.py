c15c9b098288c23957c81944d6ac46b89a73f6d9
Jevin Kardani <75773728+jevinn@users.noreply.github.com>
2024-05-20 19:50:20 +0530

    Add files via upload

---
+++ 20-5_1.py
@@ -0,0 +1,17 @@
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+fb_search_events.head()
+df=fb_search_events
+
+def calculate_rating(c):
+    if c['clicked']==0:
+        return 1
+    elif c['clicked']==1 and c['search_results_position']>3:
+        return 2
+    else:
+        return 3
+        
+df['ratings']=df.apply(lambda row: calculate_rating(row), axis=1)
+df.groupby('search_id').ratings.max().reset_index()
