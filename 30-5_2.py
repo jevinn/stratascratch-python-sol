c2de106e1d198e8aaefd6548584586317b86ee50
Jevin Kardani <75773728+jevinn@users.noreply.github.com>
2024-05-30 10:16:20 +0530

    Create 30-5_2.py

---
+++ 30-5_2.py
@@ -0,0 +1,8 @@
+# https://platform.stratascratch.com/coding/9859-find-the-first-50-records-of-the-dataset?code_type=2
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+
+worker.iloc[0:int(len(worker)/2)]
